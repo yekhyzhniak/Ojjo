77e92a1b1392b64ef418949587771b57b1ee7f65
Yevhenii Khyzhniak <keildra258@gmail.com>
2023-02-21 16:17:44 +0200

    aded btn toggle

--- _source/js/app.js
+++ _source/js/app.js
@@ -4,19 +4,38 @@ document.querySelector('.js-menu-btn').onclick = function () {
     document.querySelector('.js-menu').classList.toggle('active');
 };
 
-document.querySelector('.js-dscr-link').onclick = function (event) {
-    event.preventDefault();
-    document.querySelector('.js-dscr-link').classList.add('hidden');
-    document.querySelector('.shop__description-bg').classList.add('hidden');
-    document.querySelector('.shop__dscr').classList.add('full');
+
+if (document.querySelector('.js-dscr-link') !== null) {
+    document.querySelector('.js-dscr-link').onclick = function (event) {
+        event.preventDefault();
+        document.querySelector('.js-dscr-link').classList.add('hidden');
+        document.querySelector('.shop__description-bg').classList.add('hidden');
+        document.querySelector('.shop__dscr').classList.add('full');
+    }
 }
 
-document.querySelector('.js-show-row').onclick = function (event) {
-    event.preventDefault();
-    let shopRow = document.querySelectorAll('.shop__row');
-    for (elems of shopRow) {
-        elems.classList.remove('hidden');
+if (document.querySelector('.js-show-row') !== null) {
+    document.querySelector('.js-show-row').onclick = function (event) {
+        event.preventDefault();
+        let shopRow = document.querySelectorAll('.shop__row');
+        for (elems of shopRow) {
+            elems.classList.remove('hidden');
+        }
+        document.querySelector('.js-show-row').classList.add('hidden');
     }
-    document.querySelector('.js-show-row').classList.add('hidden');
 }
+
+
+
+if (document.querySelectorAll('.js-btn') !== null) {
+    let btn = document.querySelectorAll('.js-btn');
+    btn.forEach(function (element) {
+        element.onclick = showToogle;
+        function showToogle() {
+            element.classList.toggle('js-btn-toggle')
+        }
+    });
+}
+
+
 
